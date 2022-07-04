e9c21f0531a9d2c85bce53f259806c1dee04ed7f
Julia R <84595914+juliaRual@users.noreply.github.com>
2022-07-04 14:03:02 +0300

    date change

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,30 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter your VK token: "); //get your token from vkhost.github.io
-        String token = input.nextLine();
+//        //System.out.println("Enter your VK token: "); //get your token from vkhost.github.io
+//        String token = "vk1.a.ploGzRjmhl--L0RbWZVJk2JRsoaNGMlyu7KO3TrJ9rb8gx4Llq1Phqqst3PlHaV3PY87wCOI3IncMfJv7Eoue-y-u4JfRpq2gTokSngfIqwa8W6bYGb6EQhR7dao2Nl9n2LOxYLPFfeoPBtqg2dxoPO3w79xiro9-t0pio7T5jxKnIpczAGCYaBs-dX4jBxn";
+//
+//        System.out.println("Enter your chat id: ");
+//        String chat_id = input.nextLine();
 
-        System.out.println("Enter your chat id: ");
-        String chat_id = input.nextLine();
-
-        System.out.println("Enter new title: ");
-        String title = "С днем рождения, " + dateChecker() + "!!";
-
-        editChat(chat_id, title, token);
+//        while (true) {
+//            //triple launch for the case when several people have X day in a row
+////            chatNameEditor(chat_id, token);
+////            chatNameEditor(chat_id, token);
+////            chatNameEditor(chat_id, token);
+////            TimeUnit.SECONDS.sleep(86400);
+//
+//        }
+        dateChecker();
     }
+
 
     public static void editChat (String chat_id, String title, String token) throws IOException {
         title = title.replace(" ", "+"); //if name include spaces
@@ -39,19 +46,31 @@ class Main {
         System.out.println(response);
     }
 
+
     public static String dateChecker () {
+        int numberOfPeople = 13;
+        String[] person = new String[numberOfPeople];
+
+        person[0] = "Vlad 03.03.2022";
+        person[1] = "Today 03.07.2022";
+        person[2] = "Yulia 07.02.2022";
+
         // Get today date //
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM");
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.YYYY");
+        DateFormat nextYearDateFormat = new SimpleDateFormat("YYYY");
         Date date = new Date();
         String reDate = dateFormat.format(date);
+        String nextYear = nextYearDateFormat.format(date);
+        int nextYearInt = Integer.parseInt(nextYear) + 1;
+        System.out.println(nextYearInt);
         // Compare with array //
-        String[] person = new String[13];
-        person = peopleName();
 
-        for (int i = 0; i < 13; i++) {
+        for (int i = 0; i < numberOfPeople; i++) {
             if (person[i] != null) {
                 String[] personInfo = person[i].split(" ");
                 if (personInfo[1].equals(reDate)) {
+                    person[i] = person[0].split(".");
+                    System.out.println(person[i]);
                     return (personInfo[0]);
                 }
             }
@@ -59,16 +78,16 @@ class Main {
         return ("Original group name");
     }
 
-    public static String[] peopleName() {
-        int numberOfPeople = 13;
-        String[] person = new String[numberOfPeople];
 
-        person[0] = "Vlad 03.03";
-        person[1] = "Today 02.07";
-        person[2] = "Yulia 07.02";
+    public static void chatNameEditor(String chat_id, String token) throws IOException, InterruptedException {
+        String happyName = dateChecker();
+        if (!happyName.equals("Original group name")) {
+            String title = "С днем рождения, " + happyName + "!!";
+            editChat(chat_id, title, token);
 
-        return person;
+            TimeUnit.SECONDS.sleep(86400);
+            title = "Original group name";
+            editChat(chat_id, title, token);
+        }
     }
-
-
 }
